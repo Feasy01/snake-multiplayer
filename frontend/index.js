5d2e4baace77e66eb2374df0807a77a29e58f8ce
Feasy01 <szymon.walczak06@gmail.com>
2022-06-05 01:44:39 +0100

    added multiplyer

--- frontend/index.js
+++ frontend/index.js
@@ -7,24 +7,47 @@ const socket = io("http://localhost:3000");
 socket.on("init", handleInit);
 socket.on("gameState", handleGamestate);
 socket.on("gameOver", handleGameOver);
-
+socket.on("gameCode", handleGameCode);
+socket.on("unknownGame", handleUnknownGame);
+socket.on("tooManyPlayers", handleTooManyPlayers);
 const gameScreen = document.getElementById("gameScreen");
+const initialScreen = document.getElementById("initialScreen");
+const newGameBtn = document.getElementById("newGameButton");
+const joinGameBtn = document.getElementById("joinGameButton");
+const gameCodeInput = document.getElementById("gameCodeInput");
+const gameCodeDisplay = document.getElementById("gameCodeDisplay");
+
+newGameBtn.addEventListener("click", newGame);
+joinGameBtn.addEventListener("click", joinGame);
+function newGame() {
+	socket.emit("newGame");
+	init();
+}
+function joinGame() {
+	const code = gameCodeInput.value;
+	socket.emit("joinGame", code);
+	init();
+}
+
 let canvas, ctx;
+let playerNumber;
+let gameActive = false;
 
 function init() {
+	initialScreen.style.display = "none";
+	gameScreen.style.display = "block";
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	canvas.width = canvas.height = 600;
 	ctx.fillStyle = BG_COLOUR;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	document.addEventListener("keydown", keydown);
+	gameActive = true;
 }
 
 function keydown(e) {
 	socket.emit("keydown", e.keyCode);
 }
-
-init();
 
 function paintGame(state) {
 	ctx.fillStyle = BG_COLOUR;
@@ -34,23 +57,54 @@ function paintGame(state) {
 	const size = canvas.width / gridsize;
 	ctx.fillStyle = FOOD_COLOUR;
 	ctx.fillRect(food.x * size, food.y * size, size, size);
-	paintPlayer(state.player, size, SNAKE_COLOUR);
+	paintPlayer(state.players[0], size, SNAKE_COLOUR);
+	paintPlayer(state.players[1], size, "#FF0000");
 }
 function paintPlayer(playerState, size, colour) {
 	const snake = playerState.snake;
 	for (let cell of snake) {
-		ctx.fillStyle = SNAKE_COLOUR;
+		ctx.fillStyle = colour;
 		ctx.fillRect(cell.x * size, cell.y * size, size, size);
 	}
 }
 
-function handleInit(msg) {
-	console.log(msg);
+function handleInit(number) {
+	playerNumber = number;
 }
 function handleGamestate(gameState) {
+	if (!gameActive) {
+		return;
+	}
 	gameState = JSON.parse(gameState);
 	requestAnimationFrame(() => paintGame(gameState));
 }
-function handleGameOver() {
-	alert("you loose!");
+function handleGameOver(data) {
+	if (!gameActive) {
+		return;
+	}
+	data = JSON.parse(data);
+	if (data.winner === playerNumber) {
+		alert("You Win!");
+	} else {
+		alert("You loose!");
+	}
+	gameActive = false;
+}
+function handleGameCode(gameCode) {
+	gameCodeDisplay.innerText = gameCode;
+}
+function handleUnknownGame() {
+	reset();
+	alert("Unknown game code");
+}
+function handleTooManyPlayers() {
+	reset();
+	alert("this game is already in progres");
+}
+function reset() {
+	playerNumber = null;
+	gameCodeInput.value = "";
+	gameCodeDisplay.innerText = "";
+	initialScreen.style.display = "block";
+	gameScreen.style.display = "none";
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,61 @@
-const { createGameState, gameLoop, getUpdatedVelocity } = require("./game");
+const { initGame, gameLoop, getUpdatedVelocity } = require("./game");
+const { makeid } = require("./utils");
 const { FRAME_RATE } = require("./constants");
 const io = require("socket.io")(3000, {
 	cors: {
 		origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
 	},
 });
+
+const state = {};
+const clientRooms = {};
+
 io.on("connection", (client) => {
-	const state = createGameState();
 	client.on("keydown", handleKeydown);
+	client.on("newGame", handleNewGame);
+	client.on("joinGame", handleJoinGame);
+
+	function handleJoinGame(gameCode) {
+		const room = Array.from(io.sockets.adapter.rooms.get(gameCode));
+		console.log(room);
+		let allUsers;
+		if (room) {
+			allUsers = room;
+		}
+		let numClients = 0;
+		if (allUsers) {
+			numClients = allUsers.length;
+		}
+		if (numClients === 0) {
+			client.emit("unknownGame");
+			return;
+		} else if (numClients > 1) {
+			client.emit("tooManyPlayers");
+			return;
+		}
+		clientRooms[client.id] = gameCode;
+		client.join(gameCode);
+		client.number = 2;
+		client.emit("init", 2);
+		startGameInterval(gameCode);
+	}
+
+	function handleNewGame() {
+		let roomName = makeid(5);
+		clientRooms[client.id] = roomName;
+		client.emit("gameCode", roomName);
+		state[roomName] = initGame();
+
+		client.join(roomName);
+		client.number = 1;
+		client.emit("init", 1);
+	}
 	function handleKeydown(keyCode) {
+		const roomName = clientRooms[client.id];
+		if (!roomName) {
+			return;
+		}
+
 		try {
 			keyCode = parseInt(keyCode);
 		} catch (e) {
@@ -18,20 +65,27 @@ io.on("connection", (client) => {
 
 		const vel = getUpdatedVelocity(keyCode);
 		if (vel) {
-			state.player.vel = vel;
+			state[roomName].players[client.number - 1].vel = vel;
 		}
 	}
-	startGameInterval(client, state);
 });
-function startGameInterval(client, state) {
+function startGameInterval(roomName) {
 	const intervalId = setInterval(() => {
-		const winner = gameLoop(state);
+		const winner = gameLoop(state[roomName]);
 
 		if (!winner) {
-			client.emit("gameState", JSON.stringify(state));
+			emitGameState(roomName, state[roomName]);
 		} else {
-			client.emit("gameOver");
+			emitGameOver(roomName, winner);
+			state[roomName] = null;
 			clearInterval(intervalId);
 		}
 	}, 1000 / FRAME_RATE);
+}
+
+function emitGameState(roomName, state) {
+	io.sockets.in(roomName).emit("gameState", JSON.stringify(state));
+}
+function emitGameOver(roomName, winner) {
+	io.sockets.in(roomName).emit("gameOver", JSON.stringify({ winner }));
 }
